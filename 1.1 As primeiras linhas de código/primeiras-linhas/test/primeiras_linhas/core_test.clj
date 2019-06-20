903b643c25fe75fa4b10ad8d8e150ca5959e8a89
Peter A. Ramaldes <peter.ramaldes@gmail.com>
2019-06-20 17:12:59 -0300

    [#18] Commit inicial do primeiro projeto.

---
+++ 1.1 As primeiras linhas de código/primeiras-linhas/test/primeiras_linhas/core_test.clj
@@ -0,0 +1,7 @@
+(ns primeiras-linhas.core-test
+  (:require [clojure.test :refer :all]
+            [primeiras-linhas.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
