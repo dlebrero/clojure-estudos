32b485dea43d89a3d1223ecc22a2260af4e43860
Peter A. Ramaldes <peter.ramaldes@gmail.com>
2019-10-22 20:05:29 -0300

    Removendo arquivos do estudo anterior.

--- Capitulo 1/primeiras-linhas/src/primeiras_linhas/condicionais.clj
+++
@@ -1,9 +0,0 @@
-(ns primeiras-linhas.condicionais)
-
-; Criar uma função que exiba "Sim" se o número for par e "Não" se não for.
-(defn par? [numero]
-  (if (even? numero)
-    "Sim"
-    "Não"))
-
-(par? 2)
