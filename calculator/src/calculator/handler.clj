55ffe188c7516f04aef7db533f4349dd13b7b726
Peter A. Ramaldes <peter.ramaldes@gmail.com>
2019-10-25 14:00:58 -0300

    Refatorado a rota.

--- calculator/src/calculator/handler.clj
+++ calculator/src/calculator/handler.clj
@@ -1,9 +1,18 @@
 (ns calculator.handler
   (:require [ring.adapter.jetty :as jetty]))
 
+(defn welcome [request]
+  (if (= "/" (:uri request))
+    {:status  200
+     :body    "<h1>Hello, Clojure World</h1>
+            <p>Welcome to your first Clojure app.
+              This message is returned regardless of the request, sorry<p>"
+     :headers {}}
+    {:status  404
+     :body    "<h1>This is not the page you are looking for</h1>
+            <p>Sorry, the page you requested was not found!</p>"
+     :headers {}}))
+
 (defn -main "A very simple calculator" [port-number]
-  (jetty/run-jetty
-    (fn [request] {:status  200
-                   :body    "<h1>Hello, Clojure World</h1>  <p>Welcome to your first Clojure app.  This message is returned regardless of the request, sorry</p>"
-                   :headers {}})
-    {:port (Integer. port-number)}))
+  (jetty/run-jetty welcome
+                   {:port (Integer. port-number)}))
